eaea01267ad79269ff4f47bd084bd88a44c6987e
Hendrik Liebau <mail@hendrik-liebau.de>
2016-06-09 17:38:45 +0200

    feat: retain state if nothing changed

--- src/index.js
+++ src/index.js
@@ -3,28 +3,31 @@ import Toposort from 'toposort-class';
 export default function topologicallyCombineReducers(reducers, dependencies = {}){
     // create the toposort class
     var ts = new Toposort();
-    
+
     // add the dependencies into toposort class
     Object.keys(reducers)
         .forEach(key => {
             ts = ts.add(key, dependencies[key] || [])
         });
-        
+
     // create the processing order
     var order = ts.sort().reverse();
-    
+
     // return the combined reducer
     return (state = {}, action) => {
-        
-        // new state
-        var newState = {};
-        
-        // process the reducers
-        order.forEach(key => {
-            newState[key] = reducers[key](state[key], action, newState);
-        });
-        
-        // return the new state
-        return newState;
+
+        // process the reducers and return the newly combined state
+        return order.reduce((state, key) => {
+            var oldChildState = state[key];
+            var newChildState = reducers[key](oldChildState, action, state);
+
+            // only create a new combined state if the child state changed
+            if (oldChildState !== newChildState) {
+              return { ...state, [key]: newChildState };
+            }
+
+            // otherwise return the old state object
+            return state;
+        }, state);
     }
 }
